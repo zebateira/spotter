d97ccfd657ce8a0d4286f5a8aaed02ffa63a21ee
Zé Bateira <jlageb+github@gmail.com>
2014-01-27 23:28:05 +0000

    just for build

--- Gruntfile.js
+++ Gruntfile.js
@@ -411,7 +411,7 @@ module.exports = function (grunt) {
             options: {
                 base: 'dist'
             },
-            src: '**/*'
+            src: ['**']
         }
     });
 
---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+require.config({
+    paths: {
+        jquery: '../bower_components/jquery/jquery',
+        svgjs: '../bower_components/svg.js/dist/svg'
+    },
+    shim: {
+        bootstrap: {
+            deps: ['jquery'],
+            exports: 'jquery'
+        }
+    }
+});
+
+require(['app', 'jquery', 'foundation-amd'], function (app, jQuery) {
+    'use strict';
+
+    console.log(app);
+});
