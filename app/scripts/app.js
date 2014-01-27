27a81721b4486f0165c523c17691017e06e13ca2
Zé Bateira <jlageb+github@gmail.com>
2014-01-27 16:44:44 +0000

    preparing submission

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,12 @@
 /*global define */
-define([], function () {
+require(['jquery'], function ($) {
     'use strict';
 
-
+    $('.menu-entry').on('click', function(event) {
+      event.preventDefault();
+      
+      alert('asfd');
+    });
 
     return 'Spotter';
 });
